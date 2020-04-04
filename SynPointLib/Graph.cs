a0de803d535eab1413b8f43a5a1a01733aaafdc4
Avagr <Avagr@users.noreply.github.com>
2020-04-04 19:36:09 +0300

    Testing GraohSharp functionality

---
+++ SynPointLib/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynPointLib
+{
+    public class Graph
+    {
+        public List<Vertice> Vertices { get; }
+
+        public int EdgeCount { get; set; }
+
+        public Graph(int[,] matrix)
+        {
+
+        }
+    }
+}
--- SyncPoints/MainWindow.xaml.cs
+++ SyncPoints/MainWindow.xaml.cs
@@ -7,11 +7,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using QuickGraph.Data;
+using QuickGraph;
+using GraphSharp.Controls;
 
 namespace SyncPoints
 {
@@ -20,10 +21,36 @@ namespace SyncPoints
     /// </summary>
     public partial class MainWindow : Window
     {
+        public IBidirectionalGraph<object, IEdge<object>> GraphToVisualize { get; set; }
+
         public MainWindow()
         {
-            Console.WriteLine("HI");
+            this.DataContext = this;
+            CreateGraphToVisualize();
             InitializeComponent();
+        }
+
+        private void CreateGraphToVisualize()
+        {
+            var g = new BidirectionalGraph<object, IEdge<object>>();
+
+            //add the vertices to the graph
+            string[] vertices = new string[5];
+            for (int i = 0; i < 5; i++)
+            {
+                vertices[i] = i.ToString();
+                g.AddVertex(vertices[i]);
+            }
+
+            //add some edges to the graph
+            g.AddEdge(new Edge<object>(vertices[0], vertices[1]));
+            g.AddEdge(new Edge<object>(vertices[1], vertices[2]));
+            g.AddEdge(new Edge<object>(vertices[2], vertices[3]));
+            g.AddEdge(new Edge<object>(vertices[3], vertices[1]));
+            g.AddEdge(new Edge<object>(vertices[1], vertices[4]));
+
+            GraphToVisualize = g;
+            graphLayout.GetEdgeControl
         }
     }
 }
